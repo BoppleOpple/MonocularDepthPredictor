37aeabb14c64b53a31f6775937ad65995439ffaf
AlaskanGopher <33465052+AlaskanGopher@users.noreply.github.com>
2024-05-02 07:40:02 -0800

    zero initialization + lr tweaks

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import torch
+import torchvision
 import numpy as np
 import time
 
-a = torch.randn(3)
+a = torchvision.models.mobilenet_v2(weights='IMAGENET1K_V2')
 
-b = torch.tensor((10, 100, 1000))
-
-print(np.Infinity)
-print(time.localtime())
+print(a.features)
